643f950930648fc85ab55a3b140c4fae70e37ad5
furiousnur <furiousnur@gmail.com>
2020-02-03 18:09:36 +0600

    add ACL to laravel witout extra table

--- resources/views/admin/admin-master.blade.php
+++ resources/views/admin/admin-master.blade.php
@@ -111,16 +111,18 @@
                         </a>
                     </li>
                 </ul>
-            </li>
+            </li> 
 
-            <li class="nav-item">
-                <router-link to="/developer" class="nav-link">
-                    <i class="nav-icon fas fa-cogs"></i>
-                    <p>
-                        Developer
-                    </p>
-                </router-link>
-            </li>
+            @can('isAdmin')
+                <li class="nav-item">
+                    <router-link to="/developer" class="nav-link">
+                        <i class="nav-icon fas fa-cogs"></i>
+                        <p>
+                            Developer
+                        </p>
+                    </router-link>
+                </li>
+            @endcan
 
             <li class="nav-item">
                 <router-link to="/profile" class="nav-link">
